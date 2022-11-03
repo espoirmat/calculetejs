4d978a2d4f77952e25fda2394602b0ef56a271dd
espoir <hpmasevo@gmail.com>
2022-11-03 13:48:02 +0100

    fonction qui évalue le chiffre et renvoie à la sortie

---
+++ script.js
@@ -0,0 +1,7 @@
+         //fonction qui évalue le chiffre et renvoie la sortie
+         function calculer() 
+         { 
+             let a = document.getElementById("output").value 
+             let b = eval(a) 
+             document.getElementById("output").value = b 
+         } 
